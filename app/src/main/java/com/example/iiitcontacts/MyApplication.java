372f46ba7369a8701e5d2a5dc15415a120ec825a
Abhinav Anand <abhinav_anand@live.com>
2019-02-27 22:09:43 +0530

    Add custom Application class

---
+++ app/src/main/java/com/example/iiitcontacts/MyApplication.java
@@ -0,0 +1,28 @@
+package com.example.iiitcontacts;
+
+
+import android.app.Application;
+import android.os.StrictMode;
+
+import timber.log.Timber;
+
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyFlashScreen()
+                    .build());
+        }
+    }
+}
